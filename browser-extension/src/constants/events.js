9d3cdc2dc33a5ca9e1d0b597180bd731dab8a662
Alex Ang <E0032221@u.nus.edu>
2018-06-21 01:15:45 -0400

    Add events constants
    For managing message specification and flow

---
+++ browser-extension/src/constants/events.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018 moon
+ */
+
+/** Used to notify that the injected app was triggered by manually clicking the chrome extension icon */
+export const SOURCE_MANUAL = "MOON_SOURCE_MANUAL";
+
+/** Used to notify that the injected app was triggered by non-critical events and no rendering should occur */
+export const SOURCE_NONE = "MOON_SOURCE_NONE";
+
+/** Used to request that an injection of the moon extension onto the page */
+export const REQUEST_INJECT_APP = "MOON_REQUEST_INJECT_APP";
