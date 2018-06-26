bd0626f8ca60b409c838ab219995d0850e97e470
Alex Ang <E0032221@u.nus.edu>
2018-06-26 13:54:19 -0400

    Add clean extension script

---
+++ browser-extension/clean.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018 moon
+ */
+
+const shell = require("shelljs");
+
+const clean = () => {
+    const DIR_CACHE = `.cache/`;
+    const DIR_BUILD = `build/`;
+    console.log("Deleting cache folder...");
+    shell.rm('-r', DIR_CACHE);
+    console.log("Deleting build folder...");
+    shell.rm('-r', DIR_BUILD);
+};
+
+module.exports = clean;
+
+clean();
