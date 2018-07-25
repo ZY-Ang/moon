1386c1ba88562fbf059e38e850d3fd91440e23c3
Alex Ang <E0032221@u.nus.edu>
2018-07-25 18:17:16 -0400

    Update background emitter events
    Refactor to new shared constants folder

--- browser-extension/src/constants/events/background.js
+++ browser-extension/src/constants/events/background.js
@@ -2,6 +2,8 @@
  * Copyright (c) 2018 moon
  */
 
+// This file contains the events and request types that are emitted from the background script.
+
 /** Used to notify that the injected app was triggered by manually clicking the chrome extension icon */
 export const SOURCE_MANUAL = "MOON_SOURCE_MANUAL";
 
@@ -11,5 +13,5 @@ export const SOURCE_NONE = "MOON_SOURCE_NONE";
 /** Used to request an injection of the moon extension onto the page */
 export const REQUEST_INJECT_APP = "MOON_REQUEST_INJECT_APP";
 
-/** Used to notify of an update to the current authenticated user on the background script */
+/** Used to notify of an update to the current authenticated user from the background script */
 export const REQUEST_UPDATE_AUTH_USER = "MOON_REQUEST_UPDATE_AUTH_USER";
