f7010550c04237675af730c903ae93e348e36462
Alex Ang <E0032221@u.nus.edu>
2018-07-25 15:35:50 -0400

    Update production logger
    Reformattings

--- browser-extension/src/app/index.js
+++ browser-extension/src/app/index.js
@@ -3,12 +3,13 @@
  */
 
 if (process.env.BUILD_ENV === 'production') {
-    // TODO: Log to Cloud Service
+    // Disable client logging on production
     console.log("Pay with moon by clicking the moon icon!");
     console.log = () => {};
     console.info = () => {};
     console.warn = () => {};
     console.error = () => {};
+    // TODO: Log to Cloud Service
 } else {
     console.log(`Running in ${process.env.BUILD_ENV} environment`);
 }
