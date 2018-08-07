8f64bbe30b47bddbcf7378deea2a3d4ee3134a3d
Alex Ang <E0032221@u.nus.edu>
2018-08-07 08:25:24 -0400

    Update AWS
    Import from aws-sdk as opposed to aws-sdk/global namespace (without services) as newest sdk supports built in dynamic imports.

--- browser-extension/src/background/config/aws/AWS.js
+++ browser-extension/src/background/config/aws/AWS.js
@@ -2,7 +2,17 @@
  * Copyright (c) 2018 moon
  */
 
-import * as AWS from "aws-sdk/global";
+import * as AWS from "aws-sdk";
+/**
+ * Note: aws-sdk uses dynamic imports on a service object declaration
+ * so no additional resources are used. There is no need to handle
+ * dynamic imports for aws-sdk manually.
+ *
+ * Requiring the global namespace without services via "aws-sdk/global"
+ * is now deprecated.
+ *
+ * @see {@link https://stackoverflow.com/questions/37263336/how-to-include-only-one-class-from-aws-sdk-in-lambda}
+ */
 
 export const REGION = process.env.REGION || 'us-east-1';
 
