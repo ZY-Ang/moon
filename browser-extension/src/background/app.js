75f4825ec3b60563ec6c57c7c0c03c2e39f8eab9
Alex Ang <E0032221@u.nus.edu>
2018-08-07 03:29:17 -0400

    Update Background tabDidUpdate handler
    Catch and handle errors

--- browser-extension/src/background/app.js
+++ browser-extension/src/background/app.js
@@ -6,6 +6,7 @@ import Tabs from './browser/Tabs';
 import {isCheckoutPage, isClearCacheUrl, isValidWebUrl, isOAuthUrl, isSupportedSite} from "../utils/url";
 import {doOnAuthFlowResponse, doUpdateAuthUserEvent} from "./auth";
 import {REQUEST_INJECT_APP} from "../constants/events/background";
+import {handleErrors} from "../utils/errors";
 
 /**
  * Sends an app injection event message to the
@@ -24,36 +25,36 @@ export const doInjectAppEvent = (source) =>
 export const tabDidUpdate = (tab) => {
     if (!tab || !tab.url) {
         // Tab/URL does not exist yet - ignore and let the next call deal with it.
-        BrowserAction.setInvalidIcon();
+        BrowserAction.setInvalidIcon().catch(handleErrors);
 
     } else if (isOAuthUrl(tab.url)) {
         // URL on the current tab is a OAuth redirect URL - retrieve tokens from code grant and store in storage
-        doOnAuthFlowResponse(tab.url, tab.id);
+        doOnAuthFlowResponse(tab.url, tab.id).catch(handleErrors);
         // TODO: Let webapp domain manually handle closing so as to store tokens in localStorage as well, for dashboard purposes.
 
     } else if (isClearCacheUrl(tab.url)) {
         // URL on the current tab is the final redirect after an OAuth logout has been hit
         // DEPRECATED. An Ajax call via Axios replaced the need to manually open a new tab
-        Tabs.remove(tab);
+        Tabs.remove(tab).catch(handleErrors);
 
     } else if (!isValidWebUrl(tab.url)) {
         // URL is not of a valid web schema - e.g. chrome-extension://... or file:///...
         // We ignore and do nothing
-        BrowserAction.setInvalidIcon();
+        BrowserAction.setInvalidIcon().catch(handleErrors);
 
     } else if (isSupportedSite(tab.url)) {
         // URL on the current tab is a supported site - set to valid browser icon.
-        BrowserAction.setValidIcon(tab.id);
+        BrowserAction.setValidIcon(tab.id).catch(handleErrors);
         if (isCheckoutPage(tab.url)) {
             // URL on the current tab is supported and is a checkout page - auto render the extension
-            doInjectAppEvent(tab.url);
+            doInjectAppEvent(tab.url).catch(handleErrors);
         }
-        doUpdateAuthUserEvent();
+        doUpdateAuthUserEvent().catch(handleErrors);
 
     } else {
         // URL that is on the current tab exists and is of a valid web schema but is not a supported site
-        BrowserAction.setInvalidIcon(tab.id);
-        doUpdateAuthUserEvent();
+        BrowserAction.setInvalidIcon(tab.id).catch(handleErrors);
+        doUpdateAuthUserEvent().catch(handleErrors);
 
     }
 };
