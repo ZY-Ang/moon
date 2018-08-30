2042b181fb8dde88be53049648e9499b7326e55c
Unknown <krucraft@gmail.com>
2018-08-30 19:33:39 -0400

    Set up knex with init migration script

---
+++ backend/db/knexfile.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2018 moon
+ */
+
+// Update with your config settings.
+
+module.exports = {
+
+    development: {
+        client: 'pg',
+        connection: {
+            host: 'moonpg-cluster-1.cluster-cpezx7ml533n.us-east-1.rds.amazonaws.com',
+            user: 'moonadmin',
+            password: 'MooningMonkeys123!',
+            database: 'moon',
+            port: 5432,
+            ssl: true
+        },
+        migrations: {
+            directory: __dirname + '/migrations'
+        },
+        seeds: {
+            directory: __dirname + '/seeds/development'
+        }
+    },
+
+      staging: {
+          client: 'pg',
+          connection: {
+              host: '',
+              user: '',
+              password: '',
+              database: 'moon',
+              port: 5432,
+              ssl: true
+          },
+          migrations: {
+              directory: __dirname + '/migrations'
+          },
+          seeds: {
+              directory: __dirname + '/seeds/development'
+          }
+      },
+
+      production: {
+          client: 'pg',
+          connection: {
+              host: '',
+              user: '',
+              password: '',
+              database: 'moon',
+              port: 5432,
+              ssl: true
+          },
+          migrations: {
+              directory: __dirname + '/migrations'
+          },
+          seeds: {
+              directory: __dirname + '/seeds/development'
+          }
+      }
+
+};
