2e5b8d854ba7eaf7d18de720e6193a01ce91366b
Unknown <krucraft@gmail.com>
2018-09-04 15:51:31 -0400

    Update getWalletsForUser to get a user's wallets

--- backend/Lambda/getWalletsForUser/index.js
+++ backend/Lambda/getWalletsForUser/index.js
@@ -2,20 +2,25 @@
  * Copyright (c) 2018 moon
  */
 
+let Client = require('coinbase').Client;
+
 exports.handler = async (event) => {
-    // TODO implement coinbase get wallets for a user - this is a test
-    let wallets = [
-        {
-            id: '5',
-            userId: '12',
-            balance: '123.123'
-        },{
-            id: '5',
-            userId: '12',
-            balance: '123.123'
-        }
-    ];
-    const response = JSON.stringify(wallets);
-    console.log(response);
-    return wallets;
+
+    // todo: get the user's apikey and api secret from dynamodb
+    let client = new Client({'apiKey': mykey, 'apiSecret': mysecret});
+
+    return client.getAccounts().then((accounts) => {
+            let wallets = [];
+            accounts.forEach(account => {
+                console.log('my bal: ' + account.balance.amount + ' for ' + account.name);
+                wallets.push({
+                    balance: account.balance.amount,
+                    name: account.name
+                })
+            });
+            console.log(wallets);
+            return wallets;
+        }).catch(error => {
+            console.log('Error accessing user\'s accounts: ' + error);
+        });
 };
