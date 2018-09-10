ab24772f0ab55a8734d767dcf2b9af7338b72874
Alex Ang <E0032221@u.nus.edu>
2018-09-10 17:19:03 -0400

    Update PayTab
    Remove unused test utility

--- browser-extension/src/app/components/pay/PayTab.js
+++ browser-extension/src/app/components/pay/PayTab.js
@@ -4,7 +4,6 @@
 import React, {Component} from 'react';
 import AppRuntime from "../../browser/AppRuntime";
 import {REQUEST_TEST_FUNCTION} from "../../../constants/events/appEvents";
-import {getFormattedFileForS3} from "../../utils/file";
 
 class PayTab extends Component {
     constructor(props) {
