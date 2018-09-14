4385a239111bd6a926d4a707b0d1cd31f04cfae0
Alex Ang <E0032221@u.nus.edu>
2018-09-14 16:48:08 -0400

    Update browser-extension dev scripts
    Update outputs

--- browser-extension/dev/build.js
+++ browser-extension/dev/build.js
@@ -8,12 +8,7 @@ const fs = require("fs");
 const shell = require("shelljs");
 
 const build = async () => {
-    const DIR_BUILD = `build/`;
-    const DIR_DEV = `dev/`;
-    const DIR_SOURCE = `src/`;
-    const DIR_FILES = `files/`;
-
-    console.log("=================== BUILD CONFIGURATION ===================");
+    console.log("================== ENVIRONMENT VARIABLES ==================");
     const DEFAULT_NODE_ENV = 'development';
     console.log(`NODE_ENV:\t\t\t\t${shell.env.NODE_ENV}${!shell.env.NODE_ENV?` => ${DEFAULT_NODE_ENV}`:''}`);
     shell.env.NODE_ENV = process.env.NODE_ENV || DEFAULT_NODE_ENV;
@@ -22,11 +17,13 @@ const build = async () => {
     shell.env.AWS_PROFILE = process.env.AWS_PROFILE || DEFAULT_AWS_PROFILE;
     console.log(`AWS_ACCESS_KEY_ID:\t\t${shell.env.AWS_ACCESS_KEY_ID}`);
     console.log(`AWS_SECRET_ACCESS_KEY:\t${shell.env.AWS_SECRET_ACCESS_KEY}`);
-    const isCredentialsOverride = (shell.env.AWS_ACCESS_KEY_ID && shell.env.AWS_SECRET_ACCESS_KEY);
-    const credentials = (isCredentialsOverride)
+    const credentials = (
+        shell.env.AWS_ACCESS_KEY_ID &&
+        shell.env.AWS_SECRET_ACCESS_KEY &&
+        shell.env.CI === true // AWS.Credentials only on CI
+    )
         ? new AWS.Credentials(shell.env.AWS_ACCESS_KEY_ID, shell.env.AWS_SECRET_ACCESS_KEY)
         : new AWS.SharedIniFileCredentials({profile: shell.env.AWS_PROFILE});
-    console.log(`credentials:\t\t\t${isCredentialsOverride ? 'AWS.Credentials' : 'AWS.SharedIniFileCredentials'}`);
     shell.env.AWS_ACCOUNT_ID = await new Promise((resolve, reject) =>
         (new AWS.STS({credentials}))
             .getCallerIdentity({}, (err, data) => {
@@ -50,12 +47,21 @@ const build = async () => {
     shell.env.BROWSER = process.env.BROWSER || DEFAULT_BROWSER;
     const SOURCE_MAPS_FLAG = (shell.env.NODE_ENV === 'production') ? ' --no-source-maps' : '';
     console.log("===========================================================\n");
+    console.log("=================== BUILD CONFIGURATION ===================");
+    console.log(`credentials:\t\t\t${credentials.constructor.name}`);
+    console.log("===========================================================\n");
+
+    const DIR_BUILD = `build/`;
+    const DIR_DEV = `dev/`;
+    const DIR_SOURCE = `src/`;
+    const DIR_FILES = `files/`;
 
 // 1. Delete all build files in the build folder of the browser extension
     console.log("Clearing build folder...");
     shell.rm('-f', `${DIR_BUILD}*`);
     console.log("Force recreating build folder...");
     shell.mkdir('-p', DIR_BUILD);
+    console.log("===========================================================\n");
 
 // 2. Build react app content script
     console.log("Building content-script...");
@@ -65,6 +71,7 @@ const build = async () => {
         shell.echo('Error: build failed - unable to build chrome content script');
         shell.exit(1);
     }
+    console.log("===========================================================\n");
 
 // 3. Build background script
     console.log("Building background-script...");
@@ -74,6 +81,7 @@ const build = async () => {
         shell.echo('Error: build failed - unable to build chrome background script');
         shell.exit(1);
     }
+    console.log("===========================================================\n");
 
 // 4. Build manifest.json
     // 4i. Build the manifest builder to sync asset imports
@@ -101,10 +109,12 @@ const build = async () => {
     // 4iii. Remove the compiled manifest builder
     console.log("Removing manifestBuilder.js...");
     shell.rm('-f', PATH_MANIFEST_BUILDER_EXIT);
+    console.log("===========================================================\n");
 
 // 5. Copy all other unhashed assets
     console.log("Copying unhashed assets...");
     shell.cp('-r', `${DIR_FILES}.`, DIR_BUILD);
+    console.log("===========================================================\n");
 
 // 6. Zip folder on production to prepare for WebStore deployments
     if (shell.env.NODE_ENV === 'production') {
@@ -112,7 +122,7 @@ const build = async () => {
 
         const PATH_ZIP_EXIT = `${DIR_BUILD}moon-extension.zip`;
         const zip = new AdmZip();
-        zip.addLocalFolder(DIR_BUILD);
+        zip.addLocalFolder(DIR_BUILD, null, null);
         await new Promise((resolve, reject) =>
             zip.writeZip(PATH_ZIP_EXIT, err => {
                 if (err) {
@@ -120,7 +130,10 @@ const build = async () => {
                 }
             })
         );
+    } else {
+        console.log("Skipping folder zip...");
     }
+    console.log("===========================================================\n");
 
 // TODO: Implement publishing to chrome webstore via {@link https://developer.chrome.com/webstore/using_webstore_api}
 };
--- browser-extension/dev/clean.js
+++ browser-extension/dev/clean.js
@@ -9,8 +9,10 @@ const clean = () => {
     const DIR_BUILD = `build/`;
     console.log("Deleting cache folder...");
     shell.rm('-r', DIR_CACHE);
+    console.log("===========================================================\n");
     console.log("Deleting build folder...");
     shell.rm('-r', DIR_BUILD);
+    console.log("===========================================================\n");
 };
 
 module.exports = clean;
