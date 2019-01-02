0e3ae07939d98200b7eb6332f5c36ed2c2d00e05
Jovan <jovantechcode@gmail.com>
2019-01-02 17:11:57 -0500

    Update www.amazon.com routes

--- browser-extension/src/app/components/main/screens/www.amazon.com/constants/routes.js
+++ browser-extension/src/app/components/main/screens/www.amazon.com/constants/routes.js
@@ -2,3 +2,4 @@ export const ROUTE_AMAZON_CHECKOUT_DEFAULT = "/gp/buy/spc/handlers/display.html"
 export const ROUTE_AMAZON_CHECKOUT_DEFAULT_ADDRESS_SELECT = "/gp/buy/addressselect/handlers/display.html";
 export const ROUTE_AMAZON_CHECKOUT_DEFAULT_PAYMENT_SELECT = "/gp/buy/payselect/handlers/display.html";
 export const ROUTE_AMAZON_CHECKOUT_MUSIC = "/gp/dmusic/purchase/purchaseReview/ref=";
+export const ROUTE_AMAZON_GIFT_OPTION = "/gp/buy/gift/handlers/display.html";
