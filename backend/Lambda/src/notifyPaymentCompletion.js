d6263daca0c21e97eb9aea8fcdff2775024da9e1
Alex Ang <E0032221@u.nus.edu>
2019-01-30 00:21:56 +0800

    Update notifyPaymentCompletion
    Implement

--- backend/Lambda/src/notifyPaymentCompletion.js
+++ backend/Lambda/src/notifyPaymentCompletion.js
@@ -3,10 +3,30 @@
  */
 
 import logHead from "./utils/logHead";
+import logTail from "./utils/logTail";
+import updatePaymentPayloadRecord from "./services/paymentPayloaders/updatePaymentPayloadRecord";
 
-const notifyPaymentCompletion = (event) => {
+const notifyPaymentCompletion = async (event, context) => {
     logHead("notifyPaymentCompletion", event);
-    return true;
+
+    const {
+        logGroupName: notifyPaymentCompletionLogGroupName,
+        logStreamName: notifyPaymentCompletionLogStreamName
+    } = context;
+    const {arguments: args} = event;
+
+    const {id: paymentPayloadId, notifyPaymentCompletionPayloadInput} = args;
+    await updatePaymentPayloadRecord(paymentPayloadId, Object.assign({
+        notifyPaymentCompletionLogGroupName,
+        notifyPaymentCompletionLogStreamName
+    }, notifyPaymentCompletionPayloadInput));
+
+    const paymentCompletionPayload = {
+        id: paymentPayloadId,
+        notified: true
+    };
+    logTail("paymentCompletionPayload", paymentCompletionPayload);
+    return paymentCompletionPayload;
 };
 
 export default notifyPaymentCompletion;
