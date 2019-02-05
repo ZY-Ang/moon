a439ab41b1931ac7ed870319632bb725593b5e86
nel gutierrez <nelgucci28@gmail.com>
2019-02-05 12:47:22 -0500

    Update manifestBuilder.js
    key property will be ignored

--- browser-extension/dev/manifestBuilder.js
+++ browser-extension/dev/manifestBuilder.js
@@ -56,7 +56,7 @@ const manifest = {
         }
     ],
     //Since Firefox uses random UUIDs for web_accessible_resources, this key property is unsupported.
-    // key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAibGrYHLq3cruPQW/vkQxnHeZ+LHcE+UomNUO9QTjjdACnfpt08cBwJ52PwqBflzSre1piBUu4CB//ecW8l5zYUVPLInoGiJSsZJHA2wo2WQ9PVtRw4gJNlBQbE5AiFXTTP0DC57QGIxyfKc1E7B0pi4y3xPueVIRuhRl7cRMVAiNU2OjSKpLuNat/IpG6QTg0P1kS8QMaz9nQM83izi6k4pntisyqUthBUdvzKmP7yhJjZE8FvIozqDHtD/PPsjYA6rt1M5Gzg0a5QYgQ2NLMsKfEAWmYYnaULwtb8jwKzViflIuvnFfsQrcKjq1fqzhLICHkN+3Mb9HSOFwLaSbiwIDAQAB"
+    key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAibGrYHLq3cruPQW/vkQxnHeZ+LHcE+UomNUO9QTjjdACnfpt08cBwJ52PwqBflzSre1piBUu4CB//ecW8l5zYUVPLInoGiJSsZJHA2wo2WQ9PVtRw4gJNlBQbE5AiFXTTP0DC57QGIxyfKc1E7B0pi4y3xPueVIRuhRl7cRMVAiNU2OjSKpLuNat/IpG6QTg0P1kS8QMaz9nQM83izi6k4pntisyqUthBUdvzKmP7yhJjZE8FvIozqDHtD/PPsjYA6rt1M5Gzg0a5QYgQ2NLMsKfEAWmYYnaULwtb8jwKzViflIuvnFfsQrcKjq1fqzhLICHkN+3Mb9HSOFwLaSbiwIDAQAB"
 };
 
 // Note: You will need to destroy the compiled version of this module in the build process.
