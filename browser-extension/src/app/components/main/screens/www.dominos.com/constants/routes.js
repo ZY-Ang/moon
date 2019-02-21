c28e1eb387fd1de8ef23853bce04d096c2494065
Alex Ang <E0032221@u.nus.edu>
2019-02-22 01:36:15 +0800

    Add dominos routes
    Research on routes for dominos checkout

---
+++ browser-extension/src/app/components/main/screens/www.dominos.com/constants/routes.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2019 moon
+ */
+
+export const ROUTE_DOMINOS_ORDER_PAYMENT = "/en/pages/order/payment.jsp*";
+// TODO: Multi-language support. For now, all languages route to english.
+export const ROUTE_DOMINOS_ORDER_ADDONS = "/en/pages/order/*#!/checkout/";
+export const ROUTE_DOMINOS_ORDER_LOCATION_SEARCH = "/en/pages/order/*#!/locations/search/";
