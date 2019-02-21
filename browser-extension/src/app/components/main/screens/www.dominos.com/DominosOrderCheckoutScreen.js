7e3905c045190053b8bcf682dbc1d03e5e40b8be
Alex Ang <E0032221@u.nus.edu>
2019-02-22 01:49:52 +0800

    Add DominosOrderCheckoutScreen stub

---
+++ browser-extension/src/app/components/main/screens/www.dominos.com/DominosOrderCheckoutScreen.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019 moon
+ */
+
+import React from "react";
+
+class DominosOrderCheckoutScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                Checkout screen here
+            </div>
+        );
+    }
+}
+
+export default DominosOrderCheckoutScreen;
