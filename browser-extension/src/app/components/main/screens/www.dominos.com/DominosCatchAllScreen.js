964051774d62afb0078c7191013714512e51cfc4
Alex Ang <E0032221@u.nus.edu>
2019-02-22 01:50:40 +0800

    Add DominosCatchAllScreen
    Simply return not at checkout/order screen

---
+++ browser-extension/src/app/components/main/screens/www.dominos.com/DominosCatchAllScreen.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 moon
+ */
+
+import React from "react";
+import DominosNotAtPaymentScreen from "./DominosNotAtPaymentScreen";
+
+class DominosCatchAllScreen extends React.Component {
+    render() {
+        return <DominosNotAtPaymentScreen/>;
+    }
+}
+
+export default DominosCatchAllScreen;
