44dfe951481ea27a3f52862a540525ba3fffba47
Alex Ang <E0032221@u.nus.edu>
2019-02-22 01:49:36 +0800

    Add DominosOrderLocationSearchScreen stub

---
+++ browser-extension/src/app/components/main/screens/www.dominos.com/DominosOrderLocationSearchScreen.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 moon
+ */
+
+import React from "react";
+
+class DominosOrderLocationSearchScreen extends React.Component {
+    render() {
+        return (
+            <div>
+                Order location search
+            </div>
+        );
+    }
+}
+
+export default DominosOrderLocationSearchScreen;
